c26c6ab19fa96b04a6a768a7be156d4bc265d9ba
hxx30992670 <hxx30992670@163.com>
2016-11-12 22:22:34 +0800

    建立了第一个版本

--- drap.js
+++ drap.js
@@ -1,7 +1,17 @@
-window.onload = function(){
-	var oDiv = document.getElementById("box");
-	oDiv.onmousedown = function(){
-		var disX = oDiv.offsetLeft;
-		var disY = oDiv.offsetTop;
+function drap(id){
+	var obj = document.getElementById(id);
+	obj.onmousedown = function(){
+		var disX = obj.offsetLeft;
+		var disY = obj.offsetTop;
+		document.onmousemove = function(ev){
+			var ev = window.event || ev;
+			obj.style.left = ev.clientX - disX + 'px';
+			obj.style.top = ev.clientY - disY + 'px';
+		}
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		}
+		return false;
 	}
 }
